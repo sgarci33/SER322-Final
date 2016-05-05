9cbb6bdda8bfb04e027628933a00bb3535a39282
Seferino Fernandez <sferna12@asu.edu>
2016-05-04 18:16:35 -0700

    More SQL Table creations

---
+++ Project files/Actor.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Actor (
+    actorID INT,
+    actorName VARCHAR(20) CHARACTER SET utf8,
+    actorGender VARCHAR(6) CHARACTER SET utf8,
+    actorAge INT
+);
+INSERT INTO Actor VALUES (1,'Ethan Hawke','Male',45);
+INSERT INTO Actor VALUES (2,'Marcello Mastroianni','Male',72);
+INSERT INTO Actor VALUES (3,'Hugh Jackman','Male',47);
+INSERT INTO Actor VALUES (4,'Michael J. Fox','Male',54);
+INSERT INTO Actor VALUES (5,'Jake Gyllenhaal','Male',35);
+INSERT INTO Actor VALUES (6,'Dan Castellaneta','Male',58);
+INSERT INTO Actor VALUES (7,'Matthew McConaughey','Male',46);
+INSERT INTO Actor VALUES (8,'Colin Firth','Male',55);
+INSERT INTO Actor VALUES (9,'Emma Tompson','Female',57);
+INSERT INTO Actor VALUES (10,'Bibi Andersson','Female',80);
+INSERT INTO Actor VALUES (11,'Alexandre Rodrigues','Male',32);
+INSERT INTO Actor VALUES (12,'Miles Teller','Male',29);
+INSERT INTO Actor VALUES (13,'Robby Benson','Male',60);
+INSERT INTO Actor VALUES (14,'Joseph Cotten','Male',88);
+INSERT INTO Actor VALUES (15,'Malcolm McDowell','Male',72);
+INSERT INTO Actor VALUES (16,'Adrien Brody','Male',43);
+INSERT INTO Actor VALUES (17,'Harrison Ford','Male',73);
+INSERT INTO Actor VALUES (18,'James Robinson','Male',53);
+INSERT INTO Actor VALUES (19,'Humphrey Bogart','Male',57);
---
+++ Project files/Director.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Director (
+    directorID INT,
+    directorName VARCHAR(18) CHARACTER SET utf8,
+    directorAge INT
+);
+INSERT INTO Director VALUES (1,'Richard Linklater',55);
+INSERT INTO Director VALUES (2,'Federico Fellini',73);
+INSERT INTO Director VALUES (3,'Christopher Nolan',45);
+INSERT INTO Director VALUES (4,'Robert Zemeckis',63);
+INSERT INTO Director VALUES (5,'Richard Kelly',41);
+INSERT INTO Director VALUES (6,'David Silverman',49);
+INSERT INTO Director VALUES (7,'Tom Hooper',43);
+INSERT INTO Director VALUES (8,'Jim Sheridan',67);
+INSERT INTO Director VALUES (9,'Ingmar Bergman',89);
+INSERT INTO Director VALUES (10,'Fernando Meirelles',60);
+INSERT INTO Director VALUES (11,'Damien Chazelle',31);
+INSERT INTO Director VALUES (12,'Gary Trousdale',55);
+INSERT INTO Director VALUES (13,'Orson Welles',70);
+INSERT INTO Director VALUES (14,'Stanley Kubrick',70);
+INSERT INTO Director VALUES (15,'Roman Polanski',82);
+INSERT INTO Director VALUES (16,'Steven Spielberg',69);
+INSERT INTO Director VALUES (17,'Mel Gibson',60);
+INSERT INTO Director VALUES (18,'John Huston',81);
